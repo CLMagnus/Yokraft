2fedc84f5daa74243be4ab109ff88d3fa801d2e5
Koryn <kr.yptex@yahoo.com>
2020-04-12 23:57:20 +0200

    Finished Adamantine Set

--- src/main/java/net/mcreator/yokraft/procedures/OpenBestiaryProcedure.java
+++ src/main/java/net/mcreator/yokraft/procedures/OpenBestiaryProcedure.java
@@ -2,10 +2,9 @@ package net.mcreator.yokraft.procedures;
 
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-import net.minecraft.world.World;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.entity.Entity;
@@ -28,21 +27,16 @@ public class OpenBestiaryProcedure extends YokraftElements.ModElement {
 	}
 
 	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END) {
-			Entity entity = event.player;
-			World world = entity.world;
-			int i = (int) entity.posX;
-			int j = (int) entity.posY;
-			int k = (int) entity.posZ;
-			java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
-			dependencies.put("x", i);
-			dependencies.put("y", j);
-			dependencies.put("z", k);
-			dependencies.put("world", world);
-			dependencies.put("entity", entity);
-			dependencies.put("event", event);
-			this.executeProcedure(dependencies);
-		}
+	public void onPlayerRespawned(PlayerEvent.PlayerRespawnEvent event) {
+		Entity entity = event.getPlayer();
+		java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
+		dependencies.put("x", (int) entity.posX);
+		dependencies.put("y", (int) entity.posY);
+		dependencies.put("z", (int) entity.posZ);
+		dependencies.put("world", entity.world);
+		dependencies.put("entity", entity);
+		dependencies.put("endconquered", event.isEndConquered());
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
 	}
 }
